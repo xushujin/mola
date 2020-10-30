52a4e5b98dbe11a555ffd4b2fdaf6819db4dbc58
xushujin <286984930@qq.com>
2020-10-30 15:46:22 +0800

    多数据源切换

--- mola-bi/src/main/java/com/mola/bi/config/DataSourceConfig.java
+++ mola-bi/src/main/java/com/mola/bi/config/DataSourceConfig.java
@@ -1,32 +1,46 @@
-//package com.mola.bi.config;
-//
-//import com.alibaba.druid.pool.DruidDataSource;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//
-///**
-// * 多数据源配置
-// *
-// * @author hatim
-// */
-//@Slf4j
-//@Configuration
-//public class DataSourceConfig {
-//
-//    @Primary
-//    @Bean(name = "${spring.datasource.druid.mysql.name}", initMethod = "init")
-//    @ConfigurationProperties(prefix = "spring.datasource.druid.mysql")
-//    public DruidDataSource mysqlDs() {
-//        return new DruidDataSource();
-//    }
-//
-//
-//    @Bean(name = "${spring.datasource.druid.clickhouse.name}", initMethod = "init")
-//    @ConfigurationProperties(prefix = "spring.datasource.druid.clickhouse")
-//    public DruidDataSource clickHouseDs() {
-//        return new DruidDataSource();
-//    }
-//}
+package com.mola.bi.config;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 多数据源配置
+ *
+ * @author hatim
+ */
+@Slf4j
+@Configuration
+public class DataSourceConfig {
+
+    //    @Bean(name = "${spring.datasource.druid.mysql.name}")
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid.mysql")
+    public DataSource mysqlDs() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+
+    //    @Bean(name = "${spring.datasource.druid.clickhouse.name}")
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid.clickhouse")
+    public DataSource clickHouseDs() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public DynamicDataSource dataSource(DataSource mysqlDs, DataSource clickHouseDs) {
+        Map<Object, Object> targetDataSources = new HashMap<>(5);
+        targetDataSources.put("mysqlDs", mysqlDs);
+        targetDataSources.put("clickHouseDs", clickHouseDs);
+        return new DynamicDataSource(mysqlDs, targetDataSources);
+    }
+}
